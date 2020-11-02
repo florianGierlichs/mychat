6ccea332910366369b54c255d465f1c830518e7b
florianGierlichs <gierlichs@gmx.de>
2020-11-02 16:10:35 +0100

    Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import { useEffect } from "react";
+import Layout from "../components/Layout";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = (props: any) => {
+  useEffect(() => {
+    const getDog = async () => {
+      const response = await fetch("/api/dog");
+      const dog = await response.json();
+      console.log(dog);
+    };
+    getDog();
+  }, []);
 
-export default IndexPage
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Florian 👋</h1>
+      <p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </p>
+      <img src={props?.data?.message} />
+    </Layout>
+  );
+};
+
+export default IndexPage;
