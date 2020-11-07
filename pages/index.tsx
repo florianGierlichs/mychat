a26dea3f5bb78b1eb758346b4de102c290c0a04b
florianGierlichs <gierlichs@gmx.de>
2020-11-07 12:31:33 +0100

    Testing around

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
+import { GetStaticProps } from "next";
 import Link from "next/link";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Layout from "../components/Layout";
 
 const IndexPage = (props: any) => {
-  useEffect(() => {
-    const getDog = async () => {
-      const response = await fetch("/api/dog");
-      const dog = await response.json();
-      console.log(dog);
-    };
-    getDog();
-  }, []);
+  // useEffect(() => {
+  //   const getDog = async () => {
+  //     const response = await fetch("/api/dog");
+  //     const dog = await response.json();
+  //     console.log(dog);
+  //   };
+  //   getDog();
+  // }, []);
 
   return (
     <Layout title="Home | Next.js + TypeScript Example">
@@ -20,9 +21,19 @@ const IndexPage = (props: any) => {
           <a>About</a>
         </Link>
       </p>
-      <img src={props?.data?.message} />
+      <img src={props?.dog?.message} />
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  // Example for including static props in a Next.js function component page.
+  // Don't forget to include the respective types for any props passed into
+  // the component.
+
+  const response = await fetch("http://localhost:3000/api/dog");
+  const dog = await response.json();
+  return { props: { dog } };
 };
 
 export default IndexPage;
