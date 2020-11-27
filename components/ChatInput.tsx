65b4ed08242b920a3da243d4528716d393d09f4f
florianGierlichs <gierlichs@gmx.de>
2020-11-27 19:16:02 +0100

    Add socket io with connection to client

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import { io } from 'socket.io-client';
+const socket = io();
 
 const InputForm = styled.form``;
 const Input = styled.input`
@@ -8,8 +10,15 @@ const Input = styled.input`
 const Submit = styled.button``;
 
 export default function ChatInput(): JSX.Element {
+    socket.on('message', (message: string) => console.log(message));
+    function sendMsg(event: { preventDefault: any }) {
+        event.preventDefault();
+        console.log('submit');
+        event.preventDefault;
+    }
+
     return (
-        <InputForm>
+        <InputForm onSubmit={sendMsg}>
             <Input />
             <Submit>Submit</Submit>
         </InputForm>
--- server.ts
+++ server.ts
@@ -5,10 +5,17 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
+const server = express();
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const http = require('http').Server(server);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const io = require('socket.io')(http);
+
+// todo: remove require https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-var-requires.md
 
 const startServer = async () => {
     await app.prepare();
-    const server = express();
 
     server.get('/api/dog', async (_, res: Response) => {
         const response = await fetch('https://dog.ceo/api/breeds/image/random');
@@ -17,12 +24,17 @@ const startServer = async () => {
         return res.status(200).json(data);
     });
 
+    io.on('connection', function (socket: any) {
+        console.log('a user connected');
+        socket.emit('message', 'Hallo Welt!!!');
+    });
+
     server.all('*', (req: Request, res: Response) => {
         return handle(req, res);
     });
 
-    server.listen(port, (err?: any) => {
-        if (err) throw err;
+    http.listen(port, (err?: Error) => {
+        if (err) throw new Error(err.message);
         console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
     });
 };
