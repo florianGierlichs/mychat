0b6640fb79bc97acd0f86664b2a6c5ad96509ff5
florianGierlichs <gierlichs@gmx.de>
2020-12-23 16:47:29 +0100

    Add styling

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,16 +1,23 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
-const InputForm = styled.form``;
-
-const Input = styled.input`
-    width: 350px;
+const InputForm = styled.form`
+    display: flex;
+    justify-content: space-between;
 `;
 
-const Submit = styled.button``;
+const Input = styled.input`
+    width: -webkit-fill-available;
+    padding: 5px;
+`;
+
+const Submit = styled.button`
+    padding: 5px;
+`;
 
 export default function ChatInput({ socket }: { socket: any }): JSX.Element {
     const [chatMassage, setChatMessage] = useState('');
+    const inputEl = useRef<HTMLInputElement>(null);
 
     function onInputChange(event: any) {
         setChatMessage(event.target.value);
@@ -18,13 +25,23 @@ export default function ChatInput({ socket }: { socket: any }): JSX.Element {
 
     function sendMsg(event: { preventDefault: any }) {
         event.preventDefault();
-        socket.emit('chatMessage', chatMassage);
-        setChatMessage('');
+        if (chatMassage !== '') {
+            socket.emit('chatMessage', chatMassage);
+            setChatMessage('');
+            if (inputEl.current !== null) {
+                inputEl.current.focus();
+            }
+        }
     }
 
     return (
         <InputForm onSubmit={sendMsg}>
-            <Input onChange={onInputChange} value={chatMassage} />
+            <Input
+                onChange={onInputChange}
+                value={chatMassage}
+                ref={inputEl}
+                placeholder="message"
+            />
             <Submit>Submit</Submit>
         </InputForm>
     );
---
+++ utils/colors.ts
@@ -0,0 +1,8 @@
+const colors = {
+    primary: '#5adbbd',
+    secondary: '#2C8F78',
+    complementary: '#DB7A5A',
+    black: '#000',
+};
+
+export default colors;
